f81fe87d60cd8708d45d70e081114474b399e55d
souravpal01 <souravpal7217@gmail.com>
2023-02-15 21:02:42 +0530

    code change

--- src/main/java/com/sourav/kafka/KafkaApplication.java
+++ src/main/java/com/sourav/kafka/KafkaApplication.java
@@ -15,7 +15,9 @@ public class KafkaApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
-            kafkaTemplate.send("code", "hello sourav");
+            for (int i = 0; i < 100_00_00; i++) {
+                kafkaTemplate.send("code", "hello sourav :)");
+            }
         };
     }
 }
